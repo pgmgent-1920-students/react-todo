f24eb6d143567e853433a5ca8f003fa75a0cef89
drdynscript <drdynscript@gmail.com>
2020-04-28 10:06:58 +0200

    WIP

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Header } from './components/layout';
+import { TodoForm, TodoList } from './components/todo';
 
 import './App.css';
 
@@ -8,6 +9,10 @@ function App() {
   return (
     <div className="app">
       <Header />
+      <main className="">
+        <TodoForm />
+        <TodoList />
+      </main>
     </div>
   );
 }
